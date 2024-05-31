697766a1eb7a3e89f585f8db6ee6e2495e0e3186
Pedro Soares <pmbs.123@gmail.com>
2024-05-31 19:14:27 +0200

    feat: make fine tune up to date

--- fine_tune.py
+++ fine_tune.py
@@ -2,79 +2,51 @@ import torch
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torch.optim as optim
+from melanoma_detection import params
 from melanoma_detection.models.melanoma import MelanomaNetworkV2
-from melanoma_detection.models.base import EarlyStopping
-from melanoma_detection.transforms import SharpnessEnhancement
+from melanoma_detection.regularization import EarlyStopping
 from melanoma_detection.preprocess_dataset import (
     create_train_dataset,
     create_test_dataset,
     MelanomaDataset,
 )
+import melanoma_detection.params as params
+from melanoma_detection.transforms import TRANSFORM_TRAIN, TRANSFORM_VALIDATION
 import optuna
-
-BATCH_SIZE = 128
-EPOCHS = 30
-
-# Imagenet normalization values
-MEAN = [0.485, 0.456, 0.406]
-STD = [0.229, 0.224, 0.225]
 
 
 def objective(trial):
     # Suggest hyperparameters
     lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
-    beta1 = trial.suggest_float("beta1", 0.8, 0.999)
-    beta2 = trial.suggest_float("beta2", 0.8, 0.999)
+    # beta1 = trial.suggest_float("beta1", 0.8, 0.999)
+    # beta2 = trial.suggest_float("beta2", 0.8, 0.999)
     weight_decay = trial.suggest_float("weight_decay", 1e-10, 1e-3, log=True)
 
-    transform_train = transforms.Compose(
-        [
-            transforms.Resize(
-                (224, 224), antialias=True
-            ),  # Resize the image to 224x224 pixels
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomVerticalFlip(),
-            SharpnessEnhancement(3),
-            transforms.ToTensor(),
-            transforms.Normalize(MEAN, STD),
-        ]
-    )
-
-    transform_validation = transforms.Compose(
-        [
-            transforms.Resize((224, 224)),  # Resize the image to 224x224 pixels
-            SharpnessEnhancement(3),
-            transforms.ToTensor(),
-            transforms.Normalize(MEAN, STD),
-        ]
-    )
     train_loader = DataLoader(
-        MelanomaDataset(create_train_dataset(), transform=transform_train),
-        batch_size=BATCH_SIZE,
+        MelanomaDataset(create_train_dataset(), transform=TRANSFORM_TRAIN),
+        batch_size=params.BATCH_SIZE,
         shuffle=True,
-        num_workers=5,
+        num_workers=10,
     )
 
     test_loader = DataLoader(
-        MelanomaDataset(create_test_dataset(), transform=transform_validation),
-        batch_size=BATCH_SIZE,
+        MelanomaDataset(create_test_dataset(), transform=TRANSFORM_VALIDATION),
+        batch_size=params.BATCH_SIZE,
         shuffle=False,
-        num_workers=5,
+        num_workers=10,
     )
 
     # Create the network and define the optimizer
     net = MelanomaNetworkV2()
 
     criterion = torch.nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(
-        net.parameters(), lr=lr, betas=(beta1, beta2), weight_decay=weight_decay
-    )
+    optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
 
     # Train the network using the fit method
     net.fit(
         train_loader,
         test_loader,
-        EPOCHS,
+        params.EPOCHS,
         optimizer,
         criterion,
         EarlyStopping(5, 0.001),
@@ -88,7 +60,7 @@ def objective(trial):
 
 # Create a study object and optimize the objective function
 study = optuna.create_study(direction="maximize")
-study.optimize(objective, n_trials=10)
+study.optimize(objective, n_trials=30)
 
 print("Best trial:")
 trial = study.best_trial
