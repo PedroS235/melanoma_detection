8c0b6a59df2a28142c921a86a79a40bb287327f6
Pedro Soares <pmbs.123@gmail.com>
2024-06-02 20:28:35 +0200

    feat: split the training set into train and validation sets

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ from melanoma_detection.preprocess_dataset import (
     create_test_dataset,
     MelanomaDataset,
 )
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 import torch.optim as optim
 from melanoma_detection.models import MelanomaNetworkV2
@@ -18,40 +18,62 @@ from melanoma_detection.transforms import TRANSFORM_TRAIN, TRANSFORM_VALIDATION
 torch.manual_seed(params.SEED)
 
 
+# Load the full training dataset
+full_train_dataset = MelanomaDataset(create_train_dataset(), transform=TRANSFORM_TRAIN)
+
+# Define the validation split ratio
+validation_ratio = 0.2
+train_size = int((1 - validation_ratio) * len(full_train_dataset))
+val_size = len(full_train_dataset) - train_size
+
+# Split the dataset into training and validation sets
+train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
+
+# Create DataLoaders for training, validation, and test sets
 train_loader = DataLoader(
-    MelanomaDataset(create_train_dataset(), transform=TRANSFORM_TRAIN),
-    params.BATCH_SIZE,
+    train_dataset,
+    batch_size=params.BATCH_SIZE,
     shuffle=True,
+    num_workers=5,
+)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=params.BATCH_SIZE,
+    shuffle=False,
     num_workers=5,
 )
 
 test_loader = DataLoader(
     MelanomaDataset(create_test_dataset(), transform=TRANSFORM_VALIDATION),
-    params.BATCH_SIZE,
+    batch_size=params.BATCH_SIZE,
     shuffle=False,
     num_workers=5,
 )
 
-net = MelanomaNetworkV2()
 
-criterion = torch.nn.BCEWithLogitsLoss()
 
-optimizer = optim.Adam(
-    net.parameters(),
-    lr=0.0002288372831567034,
-    betas=(0.8378331684737104, 0.9034144582679383),
-    weight_decay=3.358130934902445e-09,
-)
+if __name__ == '__main__':
+    net = MelanomaNetworkV2()
 
-net.fit(
-    train_loader,
-    test_loader,
-    params.EPOCHS,
-    optimizer,
-    criterion,
-    EarlyStopping(5, 0.001),
-    True,
-)
+    criterion = torch.nn.BCEWithLogitsLoss()
 
-DEFAULT_PATH = f"./models/{datetime.datetime.now().time()}.pth"
-net.save(DEFAULT_PATH) if len(sys.argv) == 1 else net.save(sys.argv[1])
+    optimizer = optim.Adam(
+        net.parameters(),
+        lr=0.0002288372831567034,
+        betas=(0.8378331684737104, 0.9034144582679383),
+        weight_decay=3.358130934902445e-09,
+    )
+
+    net.fit(
+        train_loader,
+        val_loader,
+        params.EPOCHS,
+        optimizer,
+        criterion,
+        EarlyStopping(5, 0.001),
+        True,
+    )
+
+    DEFAULT_PATH = f"./models/{datetime.datetime.now().time()}.pth"
+    net.save(DEFAULT_PATH) if len(sys.argv) == 1 else net.save(sys.argv[1])
